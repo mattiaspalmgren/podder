556d0ac5707c45105a236c87470619e11e988a9a
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2017-08-07 16:59:36 +0200

    Removes lint-errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,89 @@
-'use strict';
-
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const path = require('path');
+const babelTransformSpread = require('babel-plugin-transform-object-rest-spread');
 
 const PATHS = {
   src: path.join(__dirname, 'client/src'),
   dist: path.join(__dirname, 'client/dist'),
-  style: path.join(__dirname, 'client/src')
+  style: path.join(__dirname, 'client/src'),
 };
 
-const devServer = (options) => {
-  return {
+const devServer = options => (
+  {
     devServer: {
-      historyApiFallback: true, 
-      hot: true, 
-      inline: true, 
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
       stats: 'errors-only',
       host: options.host,
-      port: options.port, 
-      contentBase: './client/dist'
+      port: options.port,
+      contentBase: './client/dist',
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin({
-        multistep: true
-      })
-    ]
-  };
-}
+        multistep: true,
+      }),
+    ],
+  }
+);
 
 // Common config among development and build
 const common = {
   entry: {
-    app: ['babel-polyfill', PATHS.src]
+    app: ['babel-polyfill', PATHS.src],
   },
   output: {
     path: PATHS.dist,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
-      { 
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            plugins: [require('babel-plugin-transform-object-rest-spread')]
-          }
-        }
+            plugins: [babelTransformSpread],
+          },
+        },
       },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: PATHS.style
+        include: PATHS.style,
       },
       {
         test: /\.svg$/,
         loader: 'svg-sprite-loader',
-        include: PATHS.style
-      }
-    ]
+        include: PATHS.style,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
+    extensions: ['.js', '.jsx'],
+  },
 };
 
 // Dynamically set up config based on environment
 let config;
-switch(process.env.NODE_ENV) {
+switch (process.env.NODE_ENV) {
   case 'build':
     config = merge(
       common,
-      { devtool: 'source-map' }
+      { devtool: 'source-map' } // eslint-disable-line
     );
     break;
+  default:
   case 'development':
     config = merge(
       common,
       { devtool: 'eval-source-map' },
       devServer({
         host: process.env.host,
-        port: 5000
-      })
+        port: 5000,
+      }) // eslint-disable-line
     );
 }
 
