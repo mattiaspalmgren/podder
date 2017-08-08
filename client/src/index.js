b84096896c158f7b9c624f2951b7d48a0fc83c03
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2017-08-08 14:09:35 +0200

    Removes Root component, and moves content to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './containers/Root';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import index from './index.scss'; // eslint-disable-line
+import icons from './plugins/icons'; // eslint-disable-line
+import configureStore from './configureStore';
+import App from './containers/App';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <Root />
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
   , document.getElementById('app'),
 );
