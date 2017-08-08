1c2c028c0a2dd7989cac5437100ee7ed56384e5d
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2017-08-08 12:38:34 +0200

    Updates eslint and gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,8 @@ module.exports = {
       "jsx-a11y/no-static-element-interactions": "off",
       "react/forbid-prop-types": "off",
       "no-underscore-dangle": "off",
+      "no-console": "off",
+      "consistent-return": "off",
+      "func-names": "off",
     }
 };
