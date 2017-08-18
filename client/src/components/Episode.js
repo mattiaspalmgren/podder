f2a3870a53e348dc4041c263e676910e04433eab
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2017-08-18 10:30:10 +0200

    Adds UI handling for longer descriptions

--- client/src/components/Episode.js
+++ client/src/components/Episode.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Svg from './Svg';
+
+const MAX_LENGTH_DESCRIPTION = 150;
 
 class Episode extends Component {
   constructor(props) {
     super(props);
+    this.toggleExpand = this.toggleExpand.bind(this);
+    this.renderDescription = this.renderDescription.bind(this);
     this.state = {
       loaded: false,
+      expanded: false,
     };
+  }
+
+  toggleExpand() {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  renderDescription() {
+    const { episode } = this.props;
+
+    const orginalDescription = episode['itunes:summary'][0];
+    const truncatedDescription = `${orginalDescription.slice(0, (MAX_LENGTH_DESCRIPTION - 3))}...`;
+    const needsExpansion = orginalDescription.length > MAX_LENGTH_DESCRIPTION;
+    const type = this.state.expanded ? 'less' : 'expand';
+    return (
+      <div>
+        { (needsExpansion && !this.state.expanded) ?
+          truncatedDescription :
+          orginalDescription
+        }
+        { needsExpansion &&
+          <span className="episode__description-icon" onClick={this.toggleExpand}>
+            <Svg color={'grey'} type={type} />
+          </span>
+        }
+      </div>
+    );
   }
 
   render() {
@@ -42,7 +74,7 @@ class Episode extends Component {
           />
         </div>
         <div className="episode__description">
-          {episode['itunes:summary']}
+          { this.renderDescription() }
         </div>
       </div>);
   }
--- client/src/plugins/icons.js
+++ client/src/plugins/icons.js
@@ -1,11 +1,15 @@
 import checkmark from '../assets/checkmark.svg';
 import cross from '../assets/cross.svg';
 import exit from '../assets/exit.svg';
+import expand from '../assets/expand.svg';
+import less from '../assets/less.svg';
 
 const icons = {
   checkmark,
   cross,
   exit,
+  expand,
+  less,
 };
 
 export default icons;
